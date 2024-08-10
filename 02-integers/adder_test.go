02885af54e49ba74c1595690ea48afe9af0f8dde
Aditya Rohman <hello.adityarohman@gmail.com>
2024-08-10 23:58:31 +0700

    complete 02-integers

---
+++ 02-integers/adder_test.go
@@ -0,0 +1,26 @@
+package integers
+
+import (
+	"fmt"
+	"testing"
+)
+
+// Add takes two integers and returns the sum of them.
+func Add(x, y int) int {
+	return x + y
+}
+
+func TestAdder(t *testing.T) {
+	sum := Add(2, 2)
+	expected := 4
+
+	if sum != expected {
+		t.Errorf("expected %d but got %d", expected, sum)
+	}
+}
+
+func ExampleAdd() {
+	sum := Add(1, 5)
+	fmt.Println(sum)
+	// Output: 6
+}
