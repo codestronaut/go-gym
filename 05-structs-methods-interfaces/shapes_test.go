bdc2251b43e8dfc8c437840c381d8bf1ba4127aa
Aditya Rohman <hello.adityarohman@gmail.com>
2024-08-11 18:10:57 +0700

    complete 05-structs-methods-interfaces

---
+++ 05-structs-methods-interfaces/shapes_test.go
@@ -0,0 +1,26 @@
+package shapes
+
+import (
+	"testing"
+)
+
+func TestArea(t *testing.T) {
+	areaTests := []struct {
+		name    string
+		shape   Shape
+		hasArea float64
+	}{
+		{name: "Rectangle", shape: Rectangle{Width: 12.0, Height: 6.0}, hasArea: 72.0},
+		{name: "Circle", shape: Circle{Radius: 10.0}, hasArea: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{Base: 12.0, Height: 6.0}, hasArea: 36.0},
+	}
+
+	for _, tt := range areaTests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+			if got != tt.hasArea {
+				t.Errorf("%#v got %g want %g", tt.shape, got, tt.hasArea)
+			}
+		})
+	}
+}
